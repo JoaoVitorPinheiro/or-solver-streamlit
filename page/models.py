baaa2207dbc5a9cec8aea5f2893be2e358a99727
JoaoVitorPinheiro <joaovpro17@gmail.com>
2022-01-16 15:54:43 -0400

    split main code in modules

--- page/models.py
+++ page/models.py
@@ -1,57 +1,298 @@
 import ortools
+import numpy as np
+import pandas as pd
 import streamlit as st
+import re
 from ortools.linear_solver import pywraplp
-from sympy import *
 
-def linear_solver(num_var, dataset):
+def create_data_model(df, coef_objetivo):
+    """Salva os dados das entradas em estruturas para processamento"""
 
-    #Inicializa um solver
-    solver = pywraplp.Solver.CreateSolver('GLOP')
+    data = {}
+
+    data['constraint_coeffs'] = np.array(df[df.columns[:-2]], dtype=float)
+    data['bounds'] = np.array(df['restricao'],dtype=float)
+    data['inequacao'] = df['sinal']
+    data['obj_coeffs'] = np.array(coef_objetivo,dtype=float)
+    data['num_vars'] = len(df.columns[:-2])
+    data['num_constraints'] = len(data['constraint_coeffs'])
     
-    #title = st.text_input('Número de variáveis', Symbol('2'))
-    #st.write('Vc escolheu:  ', title)
+    #st.write("Coeficientes das Restrições:",data['constraint_coeffs'])
+    #st.write("Termos do Lado Direito:",data['bounds'])
+    #st.write("Coeficientes da Função Objetivo:", data['obj_coeffs'])
+    #st.write("Número de Variáveis:",data['num_vars'])
+    #st.write("Número de Restriçoes:",data['num_constraints'])
 
-    x1 = solver.NumVar(0, solver.infinity(), 'x1')
-    x2 = solver.NumVar(0, solver.infinity(), 'x2')
+    return data
 
-    print('Número de variáveis =', solver.NumVariables())
+def processar_input(texto):
+    
+    regra = r"(?:(?:restricao:|r:)(?:(?<!\+|\-|\.)[\+\-\s]+(?:(?:\d+\.\d+)|(?:\d{0,9}))x\d{1,5}[\+\-\s])+(?:[=><]{1,2}[\-\+\s]+(?:(?:\d+\.\d+)|(?:\d{0,9}))+(?!\|;\w|\d)))|(?:(?:min:|max:)(?:(?<!\+|\-)[\+\-\s]+(?:(?:\d+\.\d+)|(?:\d{0,9}))x\d{1,5}[\+\-\s])+(?!\|;\w|\d))|(?:(?:problema:|p:)(?:(?<!\+|\-)[\+\-\s]+(?:inteiro|linear|int|lin)[\+\-\s]?)+(?!\|;\w|\d))|(?:(?:arredondamento:|arred:)(?:(?<!\+|\-|\.)(?:[\-\+\s]+(?:\d{1,2})(?!\|;\w|\d))))"
 
-    # Restrições
-    #1: 
-    solver.Add(x1 * 2 + x2 * 5 <= 60)
+    doc = texto
 
-    #2: 
-    solver.Add(x1 + x2 <= 18)
+    padrao = re.compile(regra, flags = re.IGNORECASE)
 
-    #3:
-    solver.Add(3*x1 + x2 <= 44)
+    coletor = [re.sub(r'[^0-9+-xzr. ]', '', item).lower() for item in padrao.findall(doc)]
 
-    #4: 
-    solver.Add(x2 <= 10)
+    restricoes = [item for item in coletor if (('restricao:' in item) or ('r:' in item))]
 
-    #5: Restrição de não-negatividade
-    solver.Add(x1 >= 0)
-    solver.Add(x2 >= 0)
+    func_obj = [item for item in coletor if (('min:' in item) or ('max:' in item))]
+    
+    arredondamento = [item for item in coletor if  (('arredondamento:' in item) or ('arred:' in item))]
 
-    print('Número de restrições =', solver.NumConstraints())
+    for item in coletor:
+        if 'max' in item:
+            objetivo = 'max'
 
-    # Função Objetivo
-    solver.Maximize(x1 * 2 + x2)
+        elif 'min' in item:
+            objetivo = 'min'
 
-    # Função Objetivo
-    solver.Maximize(x1 * 2 + x2)
+        if (('inteiro' in item) or ('int' in item)):
+            metodo = 'Programação Inteira'
 
-    status = solver.Solve()
+        elif (('linear' in item) or ('lin' in item)):
+            metodo = 'Programação Linear'
 
+    coletor = set([item[item.find(':')+1:].strip() for item in coletor if len(set(item)) != 1])
+    restricoes = [item[item.find(':')+1:].strip() for item in restricoes]
+    func_obj = [item[item.find(':')+1:].strip() for item in func_obj]
+    arredondamento = [item[item.find(':')+1:].strip() for item in arredondamento]
+    
+    try:
+        arredondamento = arredondamento[0]
+    except:
+        arredondamento = 5
+        
+    #print("\nObjetivo:")
+    #print(objetivo.upper()+"imizar".upper())
+
+    #print("\nFunção Objetivo:")
+    #print("z = ",func_obj[0])
+
+    #print("\nRestrições:")
+    #[print(item) for item in restricoes]
+
+    variaveis = set([item for item in func_obj[0].split() if "x" in item])
+
+    #print(variaveis)
+
+    for restricao in restricoes:
+        variaveis.update([item for item in restricao.split() if "x" in item])
+
+    var = []
+    for item in list(variaveis):
+        if item[0] == 'x':
+            number = 1
+            var.append(item)
+            
+        else:
+            number = item[:item.find("x")]
+            var.append(item[item.find("x"):])
+
+    var = sorted(list(set(var)))
+    #print(var)
+
+    df = pd.DataFrame(columns = var)
+
+    termos_restricao = np.array([item.split()[-1] for item in restricoes])
+    sinal = [item.split()[-2] for item in restricoes]
+
+    for item in restricoes:
+
+        lista = [i.replace(" ", "") for i in item.split()]
+
+    restr = []
+    for item in restricoes:
+            
+            lista = [i.replace(" ", "") for i in item.split()]
+            
+            for i in range(len(lista)):
+
+                if lista[i] == '-':
+                    lista[i+1] = "-"+str(lista[i+1])
+                    lista[i] = ' '
+                    
+                    i = i + 1
+
+            item = [i for i in lista if 'x' in i]
+            restr.append(item)
+
+    lista = [i.replace(" ", "") for i in func_obj[0].split()]
+
+    obj = []
+    for i in range(len(lista)):
+
+        if lista[i] == '-':
+            lista[i+1] = "-"+str(lista[i+1])
+            lista[i] = ' '
+                    
+            i = i + 1
+
+    obj = [i for i in lista if 'x' in i]
+    #print(obj)
+
+    coef_obj = []
+
+    for variavel in var:
+        number = 0
+        for item in obj:
+            if variavel in item:
+                    
+                if item[0] == 'x':
+                    number = 1
+
+                else:
+                    number = (item[:item.find("x")])                    
+
+                    if number == "-":
+                        number = -1
+
+        coef_obj.append(number)
+        
+    coef_obj = np.array(coef_obj)
+
+    for variavel in var:
+
+        walker = []
+        
+        for item in restr:
+            
+            number = 0
+            
+            for termo in item:
+                
+                if variavel in termo:
+                    
+                    if termo[0] == 'x':
+                        number = 1
+
+                    else:
+                        number = (termo[:termo.find("x")])
+                        
+                        if number == "-":
+                            number = -1
+                    
+                    #print(number)
+                    
+            walker.append(number)
+                
+        #print(walker)
+        
+        df[variavel] = np.array(walker)
+
+    df['sinal'] = sinal 
+    df['restricao'] = np.array(termos_restricao)
+    arredondamento = int(arredondamento)
+    #print(coef_obj)
+    #print(objetivo)
+    #print(df)
+
+    return df, coef_obj, objetivo, metodo, arredondamento 
+
+def solve_problem(df, coef_objetivo, metodo, objetivo, arredondamento):
+    
+    data = create_data_model(df, coef_objetivo)
+
+    x = {}
+
+    if metodo == 'Programação Linear':
+        
+        super_solver = pywraplp.Solver.CreateSolver('GLOP')
+
+        for j in range(data['num_vars']):
+            x[j] = super_solver.NumVar(0.0, super_solver.infinity(), 'x%i' % (j+1))
+
+    elif metodo == 'Programação Inteira':
+
+        super_solver = pywraplp.Solver.CreateSolver('SCIP')
+
+        for j in range(data['num_vars']):
+            x[j] = super_solver.IntVar(0, super_solver.infinity(), 'x%i' % (j+1))
+
+    st.write('Número de Variáveis =', super_solver.NumVariables())
+    
+    # Restrições de limite de valor das restrições
+    for i in range(data['num_constraints']):
+       
+        if data['inequacao'][i] == ">=":
+            limite_inferior = data['bounds'][i]
+            limite_superior = super_solver.infinity()
+            
+        elif data['inequacao'][i] == "<=":
+            limite_inferior = -super_solver.infinity()
+            limite_superior = data['bounds'][i]
+
+        else: # data['inequacao'][i] == "=="
+            limite_inferior = data['bounds'][i]
+            limite_superior = data['bounds'][i]
+
+        constraint = super_solver.RowConstraint(limite_inferior, limite_superior, '')
+
+        for j in range(data['num_vars']):
+            constraint.SetCoefficient(x[j], data['constraint_coeffs'][i][j])
+
+    st.write('Número de Restrições =', super_solver.NumConstraints())
+
+    objective = super_solver.Objective()
+
+    for j in range(data['num_vars']):
+        objective.SetCoefficient(x[j], data['obj_coeffs'][j])
+
+    if objetivo == 'max':    
+        objective.SetMaximization()
+
+    else:   
+        objective.SetMinimization()
+
+    status = super_solver.Solve()
+    #st.write(status)
     if status == pywraplp.Solver.OPTIMAL:
-        print('Solução:')
-        print('Valor Objetivo =', solver.Objective().Value())
-        print('x1 =', x1.solution_value())
-        print('x2 =', x2.solution_value())
-    else:
-        print('O problema não possui uma solução otimizada.')
 
-    print('\nTIME:')
-    print('Resolvido em %f ms' % solver.wall_time())
-    print('Resolvido em %d iterações' % solver.iterations())
+        st.balloons()
+        st.success('😻 Solução ótima encontrada!!! 😻')
+        
+        st.write('Valor Otimizado da Função Objetivo =', 
+                round(super_solver.Objective().Value(),arredondamento))
+        
+        variaveis = np.array(df.columns[:-2])
+
+        for j in range(data['num_vars']):
+            st.write(variaveis[j], ' = ', 
+                    round(x[j].solution_value(), arredondamento))
+            
+        st.write('Problema resolvido em %f ms' % super_solver.wall_time())
+        st.write('Problema resolvido em %d iterações' % super_solver.iterations())
+        st.write('Problema resolvido em %d nó(s) de branch-and-bound' % super_solver.nodes())
+
+    elif status == pywraplp.Solver.FEASIBLE:
+        st.warning('😸 Solução viável encontrada 😸')
+
+        st.write('Valor da Função Objetivo =', 
+                round(super_solver.Objective().Value(),arredondamento))
+        
+        variaveis = np.array(df.columns[:-2])
+
+        for j in range(data['num_vars']):
+            st.write(variaveis[j], ' = ', 
+                    round(x[j].solution_value(), arredondamento))
+            
+        st.write('Problema resolvido em %f ms' % super_solver.wall_time())
+        st.write('Problema resolvido em %d iterações' % super_solver.iterations())
+        st.write('Problema resolvido em %d nó(s) de branch-and-bound' % super_solver.nodes())
+    
+    elif status == pywraplp.Solver.INFEASIBLE:
+        st.warning('😿 Não há solução viável pra essa bronca 😿')
+    
+    elif status == pywraplp.Solver.UNBOUNDED:
+        st.warning('🙀 O modelo é um problema ilimitado!!! 🙀')
+
+    elif status == pywraplp.Solver.ABNORMAL:
+        st.warning('😿 Algum erro desconhecido e não conseguimos achar uma solução 😿')
+
+    elif status == pywraplp.Solver.MODEL_INVALID:
+        st.warning('😾 Modelo Inválido 😾')
+
+    else:
+        st.warning('😿 Não há solução pra essa bronca😿')
 
--- page/starter.py
+++ page/starter.py
@@ -2,6 +2,6 @@ import os
 import streamlit as st
 
 def open_streamlit():
-    os.system('streamlit run interface.py')
+    os.system('streamlit run app_teste.py')
 
     return 0 
